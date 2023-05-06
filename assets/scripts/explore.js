73bfdc5263d1e91b1d59bcbb68d3969dfa8009db
jupoon <130099925+jupoon@users.noreply.github.com>
2023-05-06 16:02:32 -0700

    explore

--- assets/scripts/explore.js
+++ assets/scripts/explore.js
@@ -3,5 +3,46 @@
 window.addEventListener('DOMContentLoaded', init);
 
 function init() {
-  // TODO
+  const synth = window.speechSynthesis;
+  const voiceSelect = document.getElementById("voice-select");
+  let voices = [];
+  
+  setTimeout(() => {
+    voices = synth.getVoices();
+    console.log(window.speechSynthesis.getVoices());
+    
+    for (let i = 0; i < voices.length; i++) {
+      const option = document.createElement("option");
+      option.textContent = `${voices[i].name} (${voices[i].lang})`;
+      voiceSelect.appendChild(option);
+    }
+  }, 50);
+
+  let speak = document.querySelector("button");
+  speak.addEventListener("click", function(){
+    var inputTxt = document.getElementById("text-to-speak");
+    const utterThis = new SpeechSynthesisUtterance(inputTxt.value);
+    const selectedOption = voiceSelect.value;
+    for (let i = 0; i < voices.length; i++) {
+      if (voices[i].name === selectedOption) {
+        utterThis.voice = voices[i];
+      }
+    }
+    synth.speak(utterThis);
+    var img = document.querySelector("header + img");    
+    setInterval(function () {
+      if(synth.speaking)
+      {
+        console.log("still here");
+        img.src = "assets/images/smiling-open.png";
+      }
+      else
+      {
+        img.src = "assets/images/smiling.png";
+        clearInterval();
+      }
+    }, 5);
+    
+    inputTxt.blur();
+  })
 }
